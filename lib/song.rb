f683a14be538c87bc5faa82c756ca84fe906c8da
Jessie Donovan <donovanajessie@gmail.com>
2020-07-11 18:09:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,18 +38,27 @@ class Song
     self.sort_by{|s| s.name}
   end
 
-#  def self.new_from_filename(file_name)
-#    parse = file_name.split (" - ")
-#    name = parse[0]
-#    song =
-
-    def self.new_from_filename(name)
-        song=self.new
-        song.name = name.split(/[.]/)[1]
-        song.artist_name =name.split(/./)[0]
-        song
-    end
+  def self.new_from_filename(file_name)
+    parse = file_name.split (" - ")
+    name = parse[0]
+    song_name = parts[1].gsub(".mp3", "")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end
 
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
-end
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
